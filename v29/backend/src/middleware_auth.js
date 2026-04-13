4303eb2d3262de68af59c5b99c58a0833aaa95b9
drwaled1999 <156463420+drwaled1999@users.noreply.github.com>
2026-04-13 11:56:51 +0300

    Update middleware_auth.js

--- v29/backend/src/middleware_auth.js
+++ v29/backend/src/middleware_auth.js
@@ -1,4 +1,4 @@
-import { db, addSecurityEvent } from './data/index.js';
+import {db} from "./data/index.js";
 import { getUserByIdRepo } from './data/userEmployeeRepository.js';
 import { verifyAccessToken } from './utils/security.js';
 
