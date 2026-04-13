0b1e4fae61806b9a2e149cdf1e5d0f91d0bf96e8
drwaled1999 <156463420+drwaled1999@users.noreply.github.com>
2026-04-13 15:58:16 +0300

    Update server.js

--- v29/backend/src/server.js
+++ v29/backend/src/server.js
@@ -7,6 +7,7 @@ import authRoutes from "./routes/authRoutes.js";
 import attendanceRoutes from "./routes/attendanceRoutes.js";
 import usersRoutes from "./routes/usersRoutes.js";
 import dashboardRoutes from "./routes/dashboardRoutes.js";
+import projectsRoutes from "./routes/projectsRoutes.js";
 
 dotenv.config();
 
@@ -15,6 +16,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use("/projects", projectsRoutes);
 
 app.get("/", (req, res) => {
   res.json({ message: "HR Portal API is running." });
