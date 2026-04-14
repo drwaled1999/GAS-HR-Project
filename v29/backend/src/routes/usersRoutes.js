f04a409a8bbafa1d05271a07a3a20dfc968501c5
drwaled1999 <156463420+drwaled1999@users.noreply.github.com>
2026-04-14 09:16:09 +0300

    Update usersRoutes.js

--- v29/backend/src/routes/usersRoutes.js
+++ v29/backend/src/routes/usersRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
-
+import express from "express";
 // GET all users
 router.get("/", async (req, res) => {
   try {
