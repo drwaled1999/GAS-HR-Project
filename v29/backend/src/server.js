61934875b0386a172cba6591b9052189cb87cc52
drwaled1999 <156463420+drwaled1999@users.noreply.github.com>
2026-04-14 14:10:27 +0300

    Update server.js

--- v29/backend/src/server.js
+++ v29/backend/src/server.js
@@ -2,6 +2,9 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 
+import { initDatabase} from "./data/database.js";
+await initDatabase();
+
 import usersRoutes from "./routes/usersRoutes.js";
 import authRoutes from "./routes/authRoutes.js";
 import dashboardRoutes from "./routes/dashboardRoutes.js";
