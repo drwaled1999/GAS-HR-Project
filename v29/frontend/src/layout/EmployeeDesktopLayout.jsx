b58bc48cc4bb2c5394d3f34ffd6fda2107501fa8
drwaled1999 <156463420+drwaled1999@users.noreply.github.com>
2026-05-04 12:25:37 +0300

    Update EmployeeDesktopLayout.jsx

--- v29/frontend/src/layout/EmployeeDesktopLayout.jsx
+++ v29/frontend/src/layout/EmployeeDesktopLayout.jsx
@@ -7,6 +7,7 @@ const links = [
   ["/", "Home"],
   ["/attendance", "Attendance"],
   ["/requests", "Requests"],
+  ["/data-update", "Data Update"],
   ["/notifications", "Notifications"],
   ["/profile", "Profile"],
 ];
